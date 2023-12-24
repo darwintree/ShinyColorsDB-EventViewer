d455f5bf14381a0807972c1d3d01520800a1822a
darwintree <daerwendeshu@outlook.com>
2023-12-24 21:13:38 +0800

    Use HarmonyOS hont

--- scripts/SharedConstants.js
+++ scripts/SharedConstants.js
@@ -1,6 +1,11 @@
-const assetUrl = "https://viewer.shinycolors.moe";
-const usedFont = "theFont";
-const zhcnFont = "AlimamaShuHeiTi";
-const translate_master_list = "https://raw.githubusercontent.com/biuuu/ShinyColors/gh-pages/story.json";
-const translate_CSV_url = 'https://raw.githubusercontent.com/biuuu/ShinyColors/gh-pages/data/story/{uid}.csv';
-const fontTimeout = 3000; // control font loading time 放弃字体加载超时时间
+const assetUrl = "https://service.sc-viewer.top/custom";
+// const assetUrl = "https://service.sc-viewer.top/convert/cache";
+// const usedFont = "theFont";
+// const zhcnFont = "AlimamaShuHeiTi";
+const usedFont = "HarmonyOS_Medium";
+const zhcnFont = "HarmonyOS_Medium";
+const translate_master_list =
+  "https://service.sc-viewer.top/raw/biuuu/ShinyColors/gh-pages/story.json";
+const translate_CSV_url =
+  "https://service.sc-viewer.top/raw/biuuu/ShinyColors/gh-pages/data/story/{uid}.csv";
+const fontTimeout = 20000; // control font loading time
